7fa2bea1308f380c2460ba663c00fb6ea485ef56
Ahmed74Khaled07 <ahmed5477422@gmail.com>
2025-04-07 22:34:08 +0200

    Add files via upload

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int alice, bob, charlie, n, total = 0;
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> alice >> bob >> charlie;
-        if (alice + bob + charlie >= 2)
-        {
-            total++;
-        }
-    }
-    cout << total << endl;
+int main()
+{
+	//C
+	 
+	 int n;
+	 cout << "Enter the number of books: ";
+	 cin >> n;
+
+	 string titles[1000];
+	 int total[1000] = {};
+	 string name;
+	 int size = 0;
+
+	 for (int i = 0; i < n; i++)
+	 {
+		 cin >> name;
+
+		 bool found = false;
+
+		 for (int j = 0; j < size; j++)
+		 {
+			 if (titles[j] == name)
+			 {
+				 total[j]++;
+				 cout << name << total[j] << endl;
+				 found = true;
+				 break;
+			 }
+		 }
+
+		 if (!found)
+		 {
+			 titles[size] = name;
+			 total[size] = 0;
+			 size++;
+			 cout << "OK" << endl;
+		 }
+	 }
 }
